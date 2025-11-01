de7e0041be8ffd71252c5b4495d262a7dfc63ff1
arafat-rahman01 <aabrarbin851@gmail.com>
2025-11-01 19:37:40 +0600

    MergeSort

--- Divide&Conquer/mergeSort.cpp
+++ Divide&Conquer/mergeSort.cpp
@@ -1,53 +1,47 @@
-#include<iostream>
-#include<vector>
+#include<bits/stdc++.h>
 using namespace std;
 
-void merge(vector<int>& arr,int st,int mid,int end){   // T.C=O(n)
-    vector<int>temp;              //Extra Space
+void merge(vector<int>& arr,int st,int mid,int end){
+    vector<int>temp;
     int i=st,j=mid+1;
-
-    while (i<=mid && j<=end)
-    {
-        if(arr[i]<=arr[j]){                 //>= Order change
+    while(i<=mid && j<=end){
+        if(arr[i]<=arr[j]){
             temp.push_back(arr[i]);
             i++;
-        }else{
+        }
+        else{
             temp.push_back(arr[j]);
             j++;
         }
     }
-    while (i<=mid)
-    {
+    while(i<=mid){
         temp.push_back(arr[i]);
         i++;
     }
-    while (j<=end)
-    {
+    while(j<=end){
         temp.push_back(arr[j]);
         j++;
     }
-    for(int idx=0;idx<temp.size();idx++){
-        arr[idx+st]=temp[idx];
+    for(int i=0;i<temp.size();i++){
+        arr[i+st]=temp[i];
     }
-    
-    
 }
 
 void mergeSort(vector<int>& arr,int st,int end){
     if(st<end){
         int mid=st+(end-st)/2;
+        mergeSort(arr,st,mid); 
+        mergeSort(arr,mid+1,end);
 
-        mergeSort(arr,st,mid);  //recurtion
-        mergeSort(arr,mid+1,end); //recurtion
+        merge(arr,st,mid,end);  
 
-        merge(arr,st,mid,end);  //Backtaking
     }
 }
 
 int main(){
-    vector<int>arr={12,31,35,8,32,17};
-    mergeSort(arr, 0, arr.size()-1);
-    for(int val:arr){
-        cout<<val<<" ";
+    vector<int>arr={12,32,11,2,65,31,56};
+    mergeSort(arr,0,arr.size()-1);
+    for(int i:arr){
+        cout<<i<<" ";
     }
 }
