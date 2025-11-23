dd16fe59f53e5822c2a0c8e2bd0adbd845e0537e
arafat-rahman01 <aabrarbin851@gmail.com>
2025-11-23 23:59:07 +0600

    optimized

--- GreedyMethod/jobSequence.cpp
+++ GreedyMethod/jobSequence.cpp
@@ -1,38 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 int main(){
-    int n;
-    cin >> n;  // number of jobs
-    vector<int> id(n), deadline(n), profit(n);
+    int n;cin>>n;
+    vector<int>id(n),deadline(n),profit(n);
+    for(int i=0;i<n;i++){
+        cin>>id[i]>>deadline[i]>>profit[i];
+    }
+     vector<pair<int,pair<int,int>>>jobs;
+     for(int i=0;i<n;i++) jobs.push_back({profit[i],{deadline[i],id[i]}});
+     sort(jobs.rbegin(),jobs.rend());
 
-    for(int i=0; i<n; i++)
-        cin >> id[i] >> deadline[i] >> profit[i];
+     int maxDeadline=0;
+     for(auto &j:jobs) maxDeadline=max(maxDeadline,j.second.first);
 
-    // sort jobs by profit (descending)
-    vector<pair<int,pair<int,int>>> jobs;
-    for(int i=0;i<n;i++) jobs.push_back({profit[i], {deadline[i], id[i]}});
-    sort(jobs.rbegin(), jobs.rend());
+     vector<int>slot(maxDeadline+1,-1);
+     int total=0;
 
-    int maxDeadline = 0;
-    for(auto &j: jobs) maxDeadline = max(maxDeadline, j.second.first);
-
-    vector<int> slot(maxDeadline+1, -1);
-    int total = 0;
-
-    for(auto &j : jobs){
-        for(int d = j.second.first; d > 0; d--){
-            if(slot[d] == -1){
-                slot[d] = j.second.second; // job id
-                total += j.first; // profit
+     for(auto &j: jobs){
+        for(int d=j.second.first;d>0;d--){
+            if(slot[d]==-1){
+                slot[d]=j.second.second;
+                total+=j.first;
                 break;
             }
         }
-    }
+     }
 
-    cout << "Jobs done: ";
-    for(int i=1; i<=maxDeadline; i++)
-        if(slot[i] != -1) cout << slot[i] << " ";
-
-    cout << "\nTotal profit: " << total << endl;
+     for(int i=1;i<=maxDeadline;i++){
+        if(slot[i]!=-1) cout<<slot[i]<<" ";
+     }
+     cout<<"Total profit: "<<total<<endl;
 }
