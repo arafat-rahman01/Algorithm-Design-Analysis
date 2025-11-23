b89e9f09ad9b56dc7a389ef777814c38728fc557
arafat-rahman01 <aabrarbin851@gmail.com>
2025-11-23 21:48:20 +0600

    update complexity

--- Graph/BFS.cpp
+++ Graph/BFS.cpp
@@ -1,42 +1,29 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-int main() {
-    int n, m;
-    cin >> n >> m;
-
-    vector<vector<int>> adj(n + 1);
-
-    // input edges
-    for (int i = 0; i < m; i++) {
-        int u, v;
-        cin >> u >> v;
+int main(){
+    int n,m;
+    cin>>n>>m;
+    vector<vector<int>>adj(n+1);
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
         adj[u].push_back(v);
-        adj[v].push_back(u); // undirected graph
+        adj[v].push_back(u);
     }
-
-    int src;
-    cin >> src;
-
-    vector<bool> visited(n + 1, false);
-    queue<int> q;
-
-    visited[src] = true;
-    q.push(src);
-
-    while (!q.empty()) {
-        int node = q.front();
+    int s;cin>>s;
+    vector<bool>visited(n+1,false);
+    queue<int>q;
+    visited[s]=true;
+    q.push(s);
+    while(!q.empty()){
+        int node=q.front();
         q.pop();
-
-        cout << node << " ";  // visiting
-
-        for (int nxt : adj[node]) {
-            if (!visited[nxt]) {
-                visited[nxt] = true;
+        cout<<node<<" ";
+        for(int nxt:adj[node]){
+            if(!visited[nxt]){
+                visited[nxt]=true;
                 q.push(nxt);
             }
         }
     }
-
-    return 0;
 }
