2474ea2e5d6447a9c1b41c8950b0a91ea707cc77
arafat-rahman01 <aabrarbin851@gmail.com>
2025-11-28 18:15:06 +0600

    Solved

---
+++ Final lab test/subArraySum.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int crossSum(vector<int>&arr,int st,int mid,int end){
+    int leftSum=INT_MIN,rightSum=INT_MIN;
+    int sum=0;
+
+    for(int i=mid;i>=st;i--){
+        sum+=arr[i];
+        leftSum=max(leftSum,sum);
+    }
+    sum=0;
+    for(int i=mid+1;i<=end;i++){
+        sum+=arr[i];
+        rightSum=max(rightSum,sum);
+    }
+    return leftSum+rightSum;
+}
+
+int findMaxSubArraySum(vector<int>&arr,int st,int end){
+    if(st==end) return arr[st];
+    int mid=(st+end)/2;
+    int leftMax=findMaxSubArraySum(arr,st,mid);
+    int rightMax=findMaxSubArraySum(arr,mid+1,end);
+    int cross=crossSum(arr,st,mid,end);
+
+    return max({leftMax,rightMax,cross});
+}
+
+int main(){
+    int n;cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<findMaxSubArraySum(arr,0,n-1)<<endl;
+}
