f722c23bfeb12780f7faca0b4262f2eb74686239
arafat-rahman01 <aabrarbin851@gmail.com>
2025-12-14 22:09:54 +0600

    TC O(n)  &  SC  O(1)

--- hashing/LongestPalindrome.cpp
+++ hashing/LongestPalindrome.cpp
@@ -1,17 +1,19 @@
-class Solution {
-public:
-    int longestPalindrome(string s) {
-        int oddCount = 0;
-        unordered_map<char, int> ump;
-        for(char ch : s) {
-            ump[ch]++;
-            if (ump[ch] % 2 == 1)
-                oddCount++;
-            else    
-                oddCount--;
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s="abccccdd";
+    int oddCount=0;
+    unordered_map<char,int>freq;
+    for(char ch:s){
+        freq[ch]++;
+        if(freq[ch]%2!=0){
+            oddCount++;
+        }else{
+            oddCount--;
         }
-        if (oddCount > 1)
-            return s.length() - oddCount + 1;
-        return s.length();
     }
-};
+    if(oddCount>1){
+        cout<<s.length()-oddCount+1;
+    }
+    else cout<<s.length();
+}
