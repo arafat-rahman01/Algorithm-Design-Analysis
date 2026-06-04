0ff5ed60e21cc1458c2a77360469d071b79e7cb8
arafat-rahman01 <aabrarbin851@gmail.com>
2026-06-04 11:33:02 +0600

    starPatterns

---
+++ Patterns1/starPatterns.java
@@ -0,0 +1,12 @@
+package Patterns1;
+
+public class starPatterns {
+    public static void main(String[] args) {
+        for(int i=0;i<=4;i++){
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
