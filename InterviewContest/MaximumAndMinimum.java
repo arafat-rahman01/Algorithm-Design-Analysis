1f694fded54c119e531e0e02f67ba8a73b800aea
arafat-rahman01 <aabrarbin851@gmail.com>
2026-06-06 16:44:05 +0600

    Mtd-02 nlog(n)

--- InterviewContest/MaximumAndMinimum.java
+++ InterviewContest/MaximumAndMinimum.java
@@ -1,6 +1,7 @@
 package InterviewContest;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class MaximumAndMinimum {
@@ -18,9 +19,18 @@ public class MaximumAndMinimum {
         return result;
     }
 
+    // public static void main(String[] args) {
+    //     int[] arr = {3,5,4,1,9};
+    //     ArrayList<Integer> result = findMinMax(arr);
+    //     System.out.println(result.get(0)+" "+result.get(1));
+    // }
+
+    // 2 
     public static void main(String[] args) {
-        int[] arr = {3,5,4,1,9};
-        ArrayList<Integer> result = findMinMax(arr);
-        System.out.println(result.get(0)+" "+result.get(1));
+        int[] arr = {5,2,7,4,8,11};
+        Arrays.sort(arr);
+        System.out.print(arr[0]+" "+arr[arr.length-1]);
     }
+
+
 }
