f73e9be807cfb681d271780cb4cff15e200878ed
arafat-rahman01 <aabrarbin851@gmail.com>
2026-06-06 16:43:13 +0600

    mth-01

---
+++ InterviewContest/test.java
@@ -0,0 +1,11 @@
+package InterviewContest;
+
+import java.util.Arrays;
+
+public class test {
+    public static void main(String[] args) {
+        int[] arr = {5,2,7,4,8,11};
+        Arrays.sort(arr);
+        System.out.print(arr[0]+" "+arr[arr.length-1]);
+    }
+}
