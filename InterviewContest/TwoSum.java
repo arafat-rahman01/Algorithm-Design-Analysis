1b2919a1b94dba40f371958b295bb9d43b1b1e9c
arafat-rahman01 <aabrarbin851@gmail.com>
2026-06-10 02:22:54 +0600

    Optimised

--- InterviewContest/TwoSum.java
+++ InterviewContest/TwoSum.java
@@ -13,3 +13,34 @@ public class TwoSum {
         System.out.println(a+" "+b);
     }
 }
+
+//https://workat.tech/problem-solving/practice/two-sum
+// class Solution {
+// 	int[] twoSum(int[] A, int target) {
+// 	    // add your logic here
+// 		int a=0,b=0;
+//         for(int i=0;i<A.length;i++){
+//             for(int j=i+1;j<A.length;j++){
+//                 if(A[i]+A[j]==t){
+//                     a=i;
+//                     b=j;
+//                 }
+//             }
+//         }
+// 		return a,b;
+// 	}
+// }
+
+//Optimised way
+// HashMap<Integer,Integer> map = new HashMap<>();
+
+// for(int i=0;i<A.length;i++){
+
+//     int need = target - A[i];
+
+//     if(map.containsKey(need)){
+//         return new int[]{map.get(need), i};
+//     }
+
+//     map.put(A[i], i);
+// }
